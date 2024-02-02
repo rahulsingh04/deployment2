456ea158e80a3d5ec9d99fb06ecec09b991d7b80
Rahul <javarahul04@gmail.com>
2024-02-02 17:25:32 +0530

    data transformation part

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,16 +1,20 @@
-import os
+## Giving Project Root Location To The Python
 import sys
+sys.path.append('E:\ml_deployment\deployment')
+
+import os
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
     train_data_path = os.path.join("Artifact", "train.csv")
-    test_data_path =  os.path.join("Artifact", "test.csv")
+    test_data_path = os.path.join("Artifact", "test.csv")
     raw_data_path = os.path.join("Artifact",  "data.csv")
 
 class DataIngestion:
@@ -26,19 +30,23 @@ class DataIngestion:
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
             logging.info("Train Test Split Initiated")
 
-            train_set,test_set = train_test_split(df , test_size=0.2, random_state=42)
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header = True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Ingest Of The Data Is Completed")
 
-            return(
+            return (
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path)
+                self.ingestion_config.test_data_path
+            )
         except Exception as e:
             raise CustomException(e, sys)
 
 if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
