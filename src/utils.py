b9870b1829a71ce09dbc7ce225d4287a05cb5840
Rahul <javarahul04@gmail.com>
2024-02-04 22:21:02 +0530

    model training with HyperParameter

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,10 @@ import os
 import sys
 import numpy as np
 import pandas as pd
-import dill
-import pickle
 from sklearn.metrics import r2_score
+import pickle
 from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -19,22 +19,36 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, X_test, Y_train, Y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for model_name, model in models.items():
-            model.fit(X_train, Y_train)  # Train model
+            parameters = param[model_name]
+
+            gs = GridSearchCV(model, parameters, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
-            train_model_score = r2_score(Y_train, y_train_pred)
-            test_model_score = r2_score(Y_test, y_test_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
 
             report[model_name] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
